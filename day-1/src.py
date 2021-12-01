3172f71a25185a4e341d6ded63d561f81fa42700
Eric Yepiz <poxtyz@gmail.com>
2021-12-01 11:04:13 -0700

    Day 1 completed

---
+++ day-1/src.py
@@ -0,0 +1,26 @@
+# Day 1
+
+def main():
+    f = open("input.txt", "r")
+    input = [x[:-1] for x in f.readlines()]
+    print("Puzzle 1's output is " + str(partOne(input)))
+    print("Puzzle 2's output is " + str(partTwo(input)))
+
+def partOne(input):
+    count = 0
+    index = 1
+    for x in input[1:]:
+        if int(x) > int(input[index -1]):
+            count = count + 1
+        index = index + 1
+    return count
+
+def partTwo(input):
+    index = 0
+    sums = []
+    for x in input[:-2]: 
+        sums.append(int(input[index]) + int(input[index+1]) + int(input[index+2]))
+        index = index + 1
+    return partOne(sums)
+
+main()
